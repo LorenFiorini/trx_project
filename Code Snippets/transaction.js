ffba01b8d85050c0a353af342101b83b746beeec
Lorenzo Fiorini <lorenfiorini@gmail.com>
2022-07-11 16:57:30 +0200

    Node modules

--- Code Snippets/transaction.js
+++ Code Snippets/transaction.js
@@ -1,25 +1,27 @@
 
-api_key = '42530830-83f7-4236-8093-30aa835d9517';
+const api_key = '42530830-83f7-4236-8093-30aa835d9517';
 
-host_url_1 = 'https://api.trongrid.io';
-host_url_2 = 'https://api.shasta.trongrid.io';
+const host_url_1 = 'https://api.trongrid.io';
+const host_url_2 = 'https://api.shasta.trongrid.io';
 
 const TronWeb = require('tronweb');
 const tronWeb = new TronWeb({
     fullHost: host_url_2,
     headers: { "TRON-PRO-API-KEY": api_key },
-    //privateKey: 'your private key'
+    privateKey: 'AD71C52E0FC0AB0DFB13B3B911624D4C1AB7BDEFAD93F36B6EF97DC955577509',
 });
+//tronWeb.setPrivateKey('42111830-fabc-4236-aaaa-0123835ddd17');
 
-to = tronWeb.address.toHex('TCyKydExSAApBFLYVyWs7G5Ye3z9F4AJpG');
-from = tronWeb.address.toHex('TUSuCUcCDARiKkoirixEsnvkZ7uiSaeqmd');
+const to = tronWeb.address.toHex('TCyKydExSAApBFLYVyWs7G5Ye3z9F4AJpG');
+const from = tronWeb.address.toHex('TUSuCUcCDARiKkoirixEsnvkZ7uiSaeqmd');
 
+console.log(to);
 // before transfer
 tronWeb.trx.getBalance(to).then(result => console.log(result));
 tronWeb.trx.getBalance(from).then(result => console.log(result));
 
 //tronWeb.trx.sendTransaction(to, 1000, from).then(result => console.log(result));
-tronWeb.trx.sendTransaction(to, 10, tronWeb.defaultPrivateKey).then(result => console.log(result));
+//tronWeb.trx.sendTransaction(to, 10).then(result => console.log(result));
 
 // after transfer
 tronWeb.trx.getBalance(to).then(result => console.log(result));
